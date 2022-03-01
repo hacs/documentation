bb4fb3ee2a75f8ad0d6474ec6222d679b8a25c86
Ludeeus <ludeeus@ludeeus.dev>
2022-03-01 16:22:03 +0000

    Add REPOSITORY_NOTES

--- script/generate_default_repositories.py
+++ script/generate_default_repositories.py
@@ -99,6 +99,11 @@ NOTES = {
     "netdaemon": NOTE_NETDAEMON,
 }
 
+
+REPOSITORY_NOTES= {
+    "zachowj_hass_node_red": "This will not give you NodeRED only an integration used by it, this is not NodeRED as that is seperate software and HACS does not install software, this is not the NodeRED add-on as [HACS does not manage add-ons](/faq/addons)"
+}
+
 resp = requests.get("https://raw.githubusercontent.com/hacs/integration/main/custom_components/hacs/utils/default.repositories")
 repositories = resp.json()
 
@@ -167,6 +172,9 @@ for category, entries in data.items():
         REPOSITORY_CONTENT += f"<p style={{{{marginBottom: 0}}}}>\nRepository: <a href='https://github.com/{entry['full_name']}' target='_blank'>{entry['full_name']}</a>\n</p>\n"
         REPOSITORY_CONTENT += "<br /><br /><br />"
         REPOSITORY_CONTENT += NOTES.get(category)
+
+        if REPOSITORY_NOTES.get(repository_id):
+            REPOSITORY_CONTENT += f":::note\n\n{REPOSITORY_NOTES[repository_id]}\n\n:::"
 
         with open(f"documentation/repositories/{entry['category']}/{repository_id}.md", "w") as mdfile:
             mdfile.write(REPOSITORY_CONTENT)
