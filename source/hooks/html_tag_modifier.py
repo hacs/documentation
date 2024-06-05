6857996305d759c1b60c5c46c850c97a71d9d678
ludeeus <joasoe@gmail.com>
2024-06-05 13:34:24 +0000

    Extemd plugin to lazy load images

---
+++ source/hooks/html_tag_modifier.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlparse
+
+import markdown
+from mkdocs.config.defaults import MkDocsConfig
+from xml.etree.ElementTree import Element
+
+EXTERNAL_SCHEMES=("http", "https")
+
+class Processor(markdown.treeprocessors.Treeprocessor):
+    def run(self, root: Element) -> Element | None:
+        for element in root.iter():
+            match element.tag:
+                case "a":
+                    if urlparse(element.get("href", "")).scheme not in EXTERNAL_SCHEMES:
+                        continue
+                    element.set("target", "_blank")
+                    element.set("rel", "noopener")
+
+                case "img":
+                    element.set("loading", "lazy")
+            
+
+class Extension(markdown.Extension):
+    def extendMarkdown(self, md: markdown.Markdown):
+        md.treeprocessors.register(item=Processor(md), name="html_tag_modifier", priority=-1000)
+
+
+def on_config(config: MkDocsConfig, **kwargs):
+    config.markdown_extensions.append(Extension())
+    return config
