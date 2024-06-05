39ef1ac328f1aae8d029080c4a57a8c444b37236
ludeeus <joasoe@gmail.com>
2024-06-05 13:00:25 +0000

    more

--- scripts/compress_images.py
+++ scripts/compress_images.py
@@ -6,6 +6,7 @@ for dirpath, dirnames, filenames in os.walk("./source/assets/images"):
    for filename in filenames:
       if filename.endswith(".png"):
         full_path = os.path.join(dirpath, filename)
-        print(f"Handling {full_path}...")
+        original_size = os.path.getsize(full_path)
         im = Image.open(full_path)
-        im.save(full_path, quality=95)
+        im.save(full_path, quality=50, optimize=True)
+        print(f"{full_path}: {original_size} -> { os.path.getsize(full_path)}")
